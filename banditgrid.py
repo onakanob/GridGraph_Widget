204ccad2fd5900bbad38a3e59f7125930a08e8fa
Oliver <onakanob@gmail.com>
2020-02-21 17:00:57 -0800

    pure SGA working.

--- banditgrid.py
+++ banditgrid.py
@@ -5,13 +5,14 @@ stochastic gradient ascent on bandit flow routers."""
 # from collections import deque
 
 import autograd.numpy as np
+from numpy.random import choice
 
 from finite_grid import Element, DiffusionGrid
 
 
 class BanditElement(Element):
-    def __init__(self, params):
-        super().__init__(params)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.H = None           # Placeholder for selection probability vector
 
     def init_H(self, length):
@@ -19,19 +20,22 @@ class BanditElement(Element):
 
     def update_target(self):
         '''choose a target from neighbors based on softmax of my P state.'''
-        softmax = lambda x: np.e ** x / np.sum(np.e ** x)
-        P = softmax(self.H)
-        # set_target takes an element
-        # np.choice will choose from the set of neighbors
+        if not self.sink:
+            softmax = lambda x: np.e ** x / np.sum(np.e ** x)
+            self.target = choice(self.neighbors, p=softmax(self.H))
 
     def update_H(self, reward, alpha):
-        pass
+        if not self.sink:
+            mask = self.target == self.neighbors
+            self.H[mask] += alpha * reward * (1 - self.H[mask])
+            self.H[~mask] -= alpha * reward * self.H[~mask]
 
 
 class BanditGrid(DiffusionGrid):
     def __init__(self, params):
         super().__init__(element_class=BanditElement, params=params)
-        self.mean_power = 0
+        self.dPs = None  # Just for safety - this should not be used currently
+        self.mean_power = 0  # Unbiased initialization
         self.reward_decay = params['reward_decay']
         self.learning_rate = params['learning_rate']
 
@@ -39,15 +43,29 @@ class BanditGrid(DiffusionGrid):
         super().init_neighbors(element)
         element.init_H(len(element.neighbors))
 
+    def generate_grid(self):
+        '''Shuffle grid connections according to local weights H.'''
+        for ele in self.elements:
+            ele.update_target()
+            ele.I = 0           # optional, just for pretty charts
+
     def power(self):
         '''override - ignore dP, add reward calculation and dissemination
         before returning y.'''
-        y = super().power()
+        Q = self.walk_graph()
+        # for i in Q:
+        #     self.elements[i].update_dP()
+        for i in reversed(Q):
+            self.elements[i].update_I()
+        y = self.sink.I * self.params['Voc'] - self.sink.debt
+        return y
+
+    def update_weights(self, y):
+        '''iterate all element weights based on return value y.'''
         reward = y - self.mean_power
-        self.mean_power += self.power_decay * reward
+        self.mean_power += self.reward_decay * reward
         for ele in self.elements:
             ele.update_H(reward, self.learning_rate)
-        return y
 
 
 if __name__ == '__main__':
@@ -55,7 +73,12 @@ if __name__ == '__main__':
 
     params = param_loader('./recipes/1 cm test.csv')
     params['elements_per_side'] = 20
+    params['reward_decay'] = 0.3
+    params['learning_rate'] = 1e-2
     
     grid = BanditGrid(params)
+    
+    my_ele = grid.elements[0]
+    
     power = grid.power()
     power = grid.power()
---
+++ run_banditgrid.py
@@ -0,0 +1,100 @@
+"""Experimental run of bandit-element solar grid optimizer."""
+import os
+import time
+import pickle
+import logging
+import argparse
+
+import autograd.numpy as np
+
+from utils import param_loader, set_logger, plot_elements, make_gif
+from banditgrid import BanditGrid
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--recipe_file', type=str,
+                        default='./recipes/1 cm test.csv',
+                        help='CSV containing run parameters.')
+    parser.add_argument('--log_dir', type=str,
+                        default='./TEMP/',
+                        help='Output directory.')
+    parser.add_argument('--resolutions', type=str,
+                        default='27',
+                        help='Comma-delim list of grid side lengths to simulate.')
+    parser.add_argument('--save_video', action='store_true', default=False,
+                        help='Save all frames and create video of simulation?')
+
+    args = parser.parse_args()
+    params = param_loader(args.recipe_file)
+    resolutions = [int(item) for item in args.resolutions.split(',')]
+
+    # Hard code ML parameters for now:
+    params['reward_decay'] = 1e-2
+    params['learning_rate'] = 10
+
+    if not os.path.exists(args.log_dir):
+        os.makedirs(args.log_dir)
+    set_logger(os.path.join(args.log_dir, 'log.txt'))
+    logging.info('Logging to dir %s', args.log_dir)
+
+    for res in resolutions:
+        t = time.time()
+        params['elements_per_side'] = res
+        model = BanditGrid(params)
+
+        save_dir = os.path.join(args.log_dir, 'model_' + str(res))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        old_power = None
+        max_iters = 1000000
+        iters = 0
+        best_power = 0
+        pkl_name = os.path.join(save_dir, 'model' + str(res) + '.pickle')
+        logging.info('Running bandit training for model with %.0f squared elements.', res)
+        while iters < max_iters:
+            model.generate_grid()
+            power = model.power()
+            model.update_weights(power)
+            iters += 1
+            if power > best_power:
+                logging.info('iter: %s -- power: %.4f', str(iters).zfill(4), power)
+                if not args.save_video:
+                    try:
+                        # with open(pkl_name, 'rb') as f:
+                        #     model.elements = pickle.load(f)
+                        plot_elements(model.element_grid(),
+                                      filename=os.path.join(save_dir,
+                                                            str(iters).zfill(4) + '.png'),
+                                      w_scale=16, i_scale=1)
+                    except IOError as err:
+                        logging.error('Best-yet grid image failed to save.')
+                        logging.error(err)
+                # try:
+                #     with open(pkl_name, 'wb') as f:
+                #         pickle.dump(model.elements, f)
+                # except IOError as err:
+                #     logging.error('Model failed save to pickle')
+                #     logging.error(err)
+
+                best_power = power
+
+            if args.save_video:
+                try:
+                    plot_elements(model.element_grid(),
+                                  filename=os.path.join(save_dir,
+                                                        str(iters).zfill(4) + '.png'),
+                                  w_scale=16, i_scale=1)
+                except IOError as err:
+                    logging.error('Image failed to save.')
+                    logging.error(err)
+
+        logging.info('Completed simulation: Model predicts %.4fW output.',
+                     best_power)
+        logging.info('Run took %.0f seconds', time.time() - t)
+        logging.info('The smallest wire width is %.6f cm.',
+                     np.min([e.get_w() for e in model.elements]))
+
+        if args.save_video:
+            make_gif(save_dir)
