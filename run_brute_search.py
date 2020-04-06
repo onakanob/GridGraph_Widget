c4842572018d4bd292c32a33a9310a3ad292fa82
Oliver <onakanob@gmail.com>
2020-04-05 18:57:20 -0700

    ran 4x4

--- run_brute_search.py
+++ run_brute_search.py
@@ -21,12 +21,16 @@ from power_handlers import lossy_handler
 from utils import param_loader, set_logger, plot_elements
 
 
-MAX_RES = 3
+MAX_RES = 4
 
 
-def graph_by_idx(idx, degrees, model):
+def graph_by_idx(idx, model, degrees=None):
     """Modify model so that it is set to the grid corresponding to index idx,
     according to a graph indexing scheme."""
+    if degrees is None:
+        degrees = np.array([len(n.neighbors) if not n.sink else 1 for n in
+                            model.elements]).astype('double')
+
     nbs = [int((idx // np.prod(degrees[0:e])) % degrees[e]) for e in range(len(degrees))]
     for i, nb in enumerate(nbs):
         model.elements[i].target = model.elements[i].neighbors[nb]
@@ -50,7 +54,7 @@ if __name__ == '__main__':
     set_logger(os.path.join(args.log_dir, 'log.txt'))
     logging.info('Logging brute force search to %s', args.log_dir)
 
-    all_results = {}
+    # all_results = {}
     for res in range(1, MAX_RES+1):
         try:
             t = time.time()
@@ -58,22 +62,33 @@ if __name__ == '__main__':
             model = DiffusionGrid(element_class=Element,
                                   solver_type=lossy_handler, params=params)
 
-            degrees = np.array([len(n.neighbors) for n in model.elements]).astype('double')
+            degrees = np.array([len(n.neighbors) if not n.sink else 1 for n in
+                                model.elements]).astype('double')
+
             results = np.zeros(degrees.prod().astype('int'))
             
-            for i in range(len(results)):
-                graph_by_idx(i, degrees, model)
-                results[i] = model.power()
+            logging.info(f'Running all {res}-square grids: {len(results)} possible.')
 
-            all_results[res] = results
+            for i, _ in enumerate(results):
+                graph_by_idx(i, model, degrees)
+                results[i] = model.power()
+                if not i%10000:
+                    logging.info(f'Completed grid {i}/{len(results)}')
+
+            # all_results[res] = results
+            logging.info(f'completed res {res} after {(time.time()-t)/60} minutes')
 
             # Parse the optimal solutions
+#            Y = np.load('4x4 brute force power returns.npy')
             plt.hist(results)
-            plt.savefig(str(res) + '_per_side_hist.png')
+            plt.savefig(os.path.join(args.log_dir, str(res) + '_per_side_hist.png'))
             best_graphs = np.argwhere(results == np.amax(results))
             for i in best_graphs:
-                graph_by_idx(i, degrees, model)
-                plot_elements(model)
+                graph_by_idx(i, model, degrees)
+                model.power()
+                plot_elements(model.element_grid(),
+                              filename=os.path.join(args.log_dir, str(res) +
+                                                    'per_' + str(i) + '.png'))
 
         except Exception as e:
             logging.error(f'failed to parse resolution {res}')
